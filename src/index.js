a47d50bae2879683e4e29903e5e9eccd898980b9
Hamichev <60568270+Hamichev@users.noreply.github.com>
2020-03-17 12:44:04 +0300

    fix: check array

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 
 exports.min = function min (array) {
+    if(!Array.isArray(array))return 0;
     if (array.length==0) {return 0;}
     return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
+    if(!Array.isArray(array))return 0;
   if (array.length==0) {return 0;}
   return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
+    if(!Array.isArray(array))return 0;
     if (array.length==0) {return 0;}
     return array.reduce((sum, current) => sum + current, 0)/array.length;
 }
